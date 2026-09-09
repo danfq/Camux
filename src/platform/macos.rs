71829d9955a6ed84123e0c163630cdb38b4e9dd8
DanFQ <dan@danfq.dev>
2026-09-09 23:44:40 +0100

    wip: add macos support (list cameras incl. wireless)

--- src/platform/macos.rs
+++ src/platform/macos.rs
@@ -1,3 +1,10 @@
+use objc2_av_foundation::{
+    AVCaptureDeviceDiscoverySession, AVCaptureDevicePosition,
+    AVCaptureDeviceTypeBuiltInWideAngleCamera, AVCaptureDeviceTypeContinuityCamera,
+    AVCaptureDeviceTypeExternal, AVMediaTypeVideo,
+};
+use objc2_foundation::NSArray;
+
 use super::{CameraBackend, CameraDevice};
 
 pub struct MacOSBackend;
@@ -10,6 +17,38 @@ impl MacOSBackend {
 
 impl CameraBackend for MacOSBackend {
     fn devices(&self) -> Result<Vec<CameraDevice>, String> {
-        Ok(vec![])
+        let (session, devices) = unsafe {
+            let media_type = AVMediaTypeVideo.ok_or("AVMediaTypeVideo is unavailable")?;
+            let device_types = NSArray::from_slice(&[
+                AVCaptureDeviceTypeBuiltInWideAngleCamera,
+                AVCaptureDeviceTypeExternal,
+                AVCaptureDeviceTypeContinuityCamera,
+            ]);
+            let session =
+                AVCaptureDeviceDiscoverySession::discoverySessionWithDeviceTypes_mediaType_position(
+                    &device_types,
+                    Some(media_type),
+                    AVCaptureDevicePosition::Unspecified,
+                );
+            let devices = session.devices();
+
+            (session, devices)
+        };
+
+        let cameras = devices
+            .iter()
+            .map(|device| {
+                let name = unsafe { device.localizedName() };
+                let id = unsafe { device.uniqueID() };
+
+                CameraDevice {
+                    id: id.to_string(),
+                    name: name.to_string(),
+                }
+            })
+            .collect();
+
+        drop(session);
+        Ok(cameras)
     }
 }
